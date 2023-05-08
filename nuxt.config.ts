02bdb370e590ed9ea6105459a5756f8090746b35
Gabriel Zerbino <gabozerbino@gmail.com>
2023-05-07 21:01:31 -0300

    Update nuxt config, added html lang attribute and icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     srcDir: 'src',
     app: {
@@ -6,6 +5,11 @@ export default defineNuxtConfig({
         head: {
           charset: 'utf-8',
           viewport: 'width=device-width, initial-scale=1',
+          htmlAttrs: { lang: 'en' },
+          link: [
+            { rel: 'icon', type: 'image/png', href: '/favicon.png'},
+            { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg'},
+          ],
         }
       },
 });
