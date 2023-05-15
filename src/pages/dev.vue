c122bdf4c248732407a8df4d914847ee2e7ca90d
Gabriel Zerbino <gabozerbino@gmail.com>
2023-05-15 11:05:53 -0300

    Adds a default dev page

---
+++ src/pages/dev.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="dev">
+        <h2>My projects</h2>
+
+        <div class="projects-wrapper">
+            <p>
+                I will be adding more projects as I finish them.
+            </p>
+        </div>
+            
+    </div>
+</template>
+
+<script setup>
+
+definePageMeta({
+    layout: 'default',
+});
+
+useHead({
+    title: 'My Work',
+});
+
+</script>
+
+<style scoped>
+
+</style>
